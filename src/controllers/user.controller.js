e28c73e316a1fe8d6373c58df8c6274a74202e76
golubhattuk01 <148994632+golubhattuk01@users.noreply.github.com>
2024-03-17 22:22:20 +0530

    remove console logs

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -178,7 +178,6 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
   try {
     const incomingRefreshToken =
       req.cookies.refreshToken || req.body.refreshToken;
-    console.log("1");
     if (!incomingRefreshToken) {
       throw new ApiError(401, "Unauthorised Request");
     }
